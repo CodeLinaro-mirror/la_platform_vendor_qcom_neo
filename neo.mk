1f9b09c03076567d24de43c3d9b7ea7ae4629931
Divisha Bisht <quic_divibish@quicinc.com>
2022-07-27 16:14:39 +0530

    neo: Add feature specific flags for SMCInvoke and QSEECom
    Add TARGET_QSEECOM_DISABLED and TARGET_SMCINOKE_DISABLED feature
    specific flags to disable QSEECom feature and SMCInvoke feature
    respectively for the target.
    
    Change-Id: I93335252a9c764a1ce2b5f9d3a1537b5cf5deaf4

--- neo.mk
+++ neo.mk
@@ -532,6 +532,10 @@ PRODUCT_VENDOR_PROPERTIES+= \
 # GPU Profiler support
 PRODUCT_VENDOR_PROPERTIES += graphics.gpu.profiler.support=true
 
+# Disable QSEECom feature
+TARGET_QSEECOM_DISABLED := true
+# Disable SMCInvoke feature
+TARGET_SMCINVOKE_DISABLED := true
 
 ##############################Go configs###########################################
 
