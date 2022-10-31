e7e5c22fce19467a2acb875a714d6a3c0cf2068b
Divisha Bisht <quic_divibish@quicinc.com>
2022-10-31 10:49:51 +0530

    neo: Add feature flag TARGET_ENABLE_SMCI_SYSLISTENER
    Add new feature flag TARGET_ENABLE_SMCI_SYSLISTENER for
    enabling smcinvoke based listeners framework and
    enable the flag for Aurora LA.
    Updating the feature flags for qseecom and smcinvoke.
    
    Change-Id: I0110a9cf76e9678ed432c19780fc0871ffe74717

--- neo.mk
+++ neo.mk
@@ -532,10 +532,8 @@ PRODUCT_VENDOR_PROPERTIES+= \
 # GPU Profiler support
 PRODUCT_VENDOR_PROPERTIES += graphics.gpu.profiler.support=true
 
-# Disable QSEECom feature
-TARGET_QSEECOM_DISABLED := true
-# Disable SMCInvoke feature
-TARGET_SMCINVOKE_DISABLED := true
+# Enable Smcinvoke based System Listeners
+TARGET_ENABLE_SMCI_SYSLISTENER := true
 
 ##############################Go configs###########################################
 
